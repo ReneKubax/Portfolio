179cc3c369b3046fdd08b6c2025eef1248b82775
ReneKubax <therene21@gmail.com>
2023-05-12 15:11:03 -0600

    Installing packages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
+import { Navbar } from '../../components/Navbar'
 import './globals.css'
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Renes Portfolio',
@@ -15,7 +14,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <Navbar/>
+        {children}
+        </body>
     </html>
   )
 }
