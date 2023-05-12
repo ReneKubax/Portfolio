f9bfe7aa076cef9b58b176051eb2b387b33e1174
ReneKubax <therene21@gmail.com>
2023-05-12 16:21:33 -0600

    change style name navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -39,7 +39,7 @@ const NAV_ITEMS: Array<NavItem> = [
             <div className="flex items-center justify-between py-3 md:py-5 md:block">
               <Link to="home">
                 <div className="container flex items-center space-x-2">
-                  <h2 className="">Rene Fuentes</h2>
+                  <h2 className="text-2xl font-bold">Rene Fuentes</h2>
                 </div>
               </Link>
               <div className="md:hidden">
