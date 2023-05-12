758207fbffaa733617492b1aaaadf8d59d8812fe
ReneKubax <therene21@gmail.com>
2023-05-12 15:15:03 -0600

    Config Next File and changes in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,9 +7,15 @@ import { useTheme } from "next-themes"
 import { RiMoonFill, RiSunLine } from "react-icons/ri"
 import { IoMdMenu, IoMdClose } from "react-icons/io"
 
+interface NavItem {
+    label: string
+    page: string
+}
 
-export const Navbar = () => {
+const Navbar = () => {
   return (
     <div>Navbar</div>
   )
 }
+
+export default Navbar
